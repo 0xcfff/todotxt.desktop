8c32b9a316ea45b1fd5212da129966ff72ea5a1a
Mikhail Tyuzin <0xcfff@gmail.com>
2025-10-05 13:50:04 +0300

    Remove unneeded scripts

--- coverage.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-# Create reports/coverage directory if it doesn't exist
-mkdir -p reports/coverage
-
-dotnet test src/TodoTxt.Lib.Tests/ToDoTests.csproj /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura /p:CoverletOutput=reports/coverage/baseline.xml
